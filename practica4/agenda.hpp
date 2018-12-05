a2c71b2cd772cd5963abef9cdf844f5310a5bdc9
Alex Ariza <alex1995ariza@gmail.com>
2018-12-05 10:06:51 +0100

    Fixed agenda.cpp

--- practica4/agenda.hpp
+++ practica4/agenda.hpp
@@ -1,1 +1,53 @@
 //clase agenda
+
+#ifndef AGENDA_HPP
+#define  AGENDA_HPP
+
+#include <iostream>
+#include <vector>
+#include "alumno.hpp"
+#include "grupo.hpp"
+using namespace std;
+
+class Agenda{
+private:
+  std::vector<Alumno> vAlumnos;
+  std::vector<Grupo> vGrupos;
+
+public:
+
+    Agenda()
+    {
+      setvAlumnos(NULL);
+      setvGrupos(NULL);
+    }
+
+    inline void setvAlumnos(std::vector<Alumno> v)
+    {
+      for(int i=0; i<v.size(); i++)
+      {
+        v[i]=vAlumnos[i];
+      }
+    }
+
+    inline void setvGrupos(std::vector<Grupo> v)
+    {
+      for(int i=0; i<v.size(); i++)
+      {
+        v[i]=vGrupos[i];
+      }
+    }
+
+    inline std::vector<Alumno> getVectorAlumnos()
+    {
+      return vAlumnos;
+    }
+
+    inline std::vector<Grupo> getVectorGrupos()
+    {
+      return vGrupos;
+    }
+};
+
+
+#endif
