f03159a618d344af49c9991bf6260944ef9947dd
Javibu5 <adfjb77@gmail.com>
2018-12-05 11:19:12 +0100

    added search add delete

--- practica4/agenda.cpp
+++ practica4/agenda.cpp
@@ -1,5 +1,34 @@
 //funciones clase agenda
 #include "agenda.hpp"
 
-bool 
-void addAlumno(){}
+
+int searchAlumno(string dni){
+
+     for(int i=0; i<vAlumnos.size(); i++)
+      {
+       if(vAlumnos[i].getDNI().compare(dni) == 0){
+         return i;
+       }
+      }
+      return 0;
+
+};
+
+
+void addAlumno(Alumno alumno){
+  if(vAlumnos.size > 151){
+    if(searchAlumno(alumno.getDni()) == 0){
+      vAlumnos.push_back(alumno);}
+        }
+        else
+        {
+      printf("La lista de alumnos está llena");}
+      };
+
+void deleteAlumno(Alumno alumno){
+  if(!vAlumnos.empty()){
+  vAlumnos.pop_back();
+  }else{
+    printf("La lista está vacia");
+  }
+};
